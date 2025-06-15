7c66410a355e06b4f3a3a34a0fa37b4be4f93521
dirt <dirtstump@gmail.com>
2025-06-15 12:39:19 -0500

    ai agent using google gemini (boot.dev course)

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+def main():
+    if len(sys.argv) < 2 or sys.argv[1][0] == "-":
+        print("error: no prompt given")
+        sys.exit(1)
+
+    user_prompt = sys.argv[1]
+
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    from google import genai
+    from google.genai import types
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+    return
+
+if __name__ == "__main__":
+    main()
